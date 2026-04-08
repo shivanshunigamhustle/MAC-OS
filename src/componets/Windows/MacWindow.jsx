a7edfa8283fe08874cf89da90e87aee3a4f59416
shivanshunigam <shivanshunigam6@gmail.com>
2026-04-08 15:10:00 +0530

    done done

--- src/componets/Windows/MacWindow.jsx
+++ src/componets/Windows/MacWindow.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import  {Rnd}  from 'react-rnd'
-import "./window.scss"
+import "./Window.scss"
 
 const MacWindow = ({ children, width = "40vw", height = "40vh", windowName, setWindowsState }) => {
     return (
